dcd7e6e9c6707031fc590741d4f2d5554e41ba2d
xiaoyaoyouyue <zhaowei1295128@qq.com>
2021-01-10 15:51:15 +0800

    change file name by auther：zhaoyuxiao

---
+++ PythonLearning/python 100 days/loadlib.py
@@ -0,0 +1,9 @@
+# -*-coding: utf-8-*-
+# @Time    : 2021/1/10-15:46
+# @Auther  : zhaoyuxiao
+# @File    : 1.6.2 loadlib.py
+# @Software: PyCharm
+
+
+
+h = mylib.Hello
---
+++ PythonLearning/python 100 days/mylib.py
@@ -0,0 +1,9 @@
+# -*-coding: utf-8-*-
+# @Time    : 2021/1/10-15:47
+# @Auther  : zhaoyuxiao
+# @File    : 1.6.1 mylib.py
+# @Software: PyCharm
+
+class Hello:
+    def sayHello(self):
+        print("say Hello!")
