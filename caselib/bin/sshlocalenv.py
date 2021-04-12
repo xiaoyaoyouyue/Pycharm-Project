5b983f62d8d34851a14a850e44f344215573f30a
xiaoyaoyouyue <zhaowei1295128@qq.com>
2021-04-13 00:01:15 +0800

    登录节点脚本

---
+++ caselib/bin/sshlocalenv.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/4/12
+# @Auther  : zhaoyuxiao
+# @File    : sshlocalenv.py
+# @Software: PyCharm
+
+
+import paramiko
+
+# 创建SSH对象
+ssh = paramiko.SSHClient()
+
+# 把要连接的机器添加到known_hosts文件中
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+# 连接服务器
+ssh.connect(hostname='192.168.106.130', port=22, username='root', password='xiaoyao45682')
+
+# cmd = 'ps'
+cmd = 'ls -l;ifconfig'       # 多个命令用;隔开
+stdin, stdout, stderr = ssh.exec_command(cmd)
+
+result = stdout.read()
+
+if not result:
+    result = stderr.read()
+ssh.close()
+
+print(result.decode())
+
+
